baf4cd9dad80efec03b114187faf9814759bb660
moFuhidy <mo7ammed9290@gmail.com>
2023-06-05 13:15:47 +0300

    refactor to ES6 modules
    added luxon module

--- modules/timeDate.js
+++ modules/timeDate.js
@@ -1,14 +1,15 @@
-import { DateTime } from 'luxon';
+import { DateTime } from './luxon.js';
 
-const now = DateTime.now();
 const showTime = () => {
   const dateTime = document.querySelector('.time');
   const timeDate = () => {
     setInterval(() => {
-      const date = new Date().toUTCString();
-      dateTime.innerHTML = now;
+      const dt = DateTime.utc().toLocal();
+      dateTime.innerHTML = dt.toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
     }, 0);
   };
 
   timeDate();
 };
+
+export default showTime;
